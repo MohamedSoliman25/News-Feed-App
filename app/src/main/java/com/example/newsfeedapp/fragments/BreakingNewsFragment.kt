363b3b5ffe6380dbd47931a23eb0293882761345
Mohamed Soliman <mohamedelzmar2018@gmail.com>
2022-06-11 10:14:54 +0200

    Bottom navigation completed

---
+++ app/src/main/java/com/example/newsfeedapp/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.newsfeedapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsfeedapp.R
+
+
+class BreakingNewsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_breaking_news, container, false)
+    }
+}
+
